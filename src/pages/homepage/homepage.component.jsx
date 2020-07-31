20aea02ba0ecb6c7786146b975aef979324424b7
unknown <santoshbist69@gmail.com>
2020-07-31 22:22:56 +0545

    We just start

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './homepage.styles.css';
+const HomePage = () => (
+    <div className='home-page'>
+        <h1>Hello world</h1>
+    </div>
+)
+export default HomePage;
